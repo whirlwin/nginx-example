f160341785bb542b6d7074202438dcde732345c0
whirlwin <whirlwin90@gmail.com>
2013-11-26 20:52:56 +0100

    Make start script more robust.
    - Add startup config

--- start.sh
+++ start.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/sh
 
+WWW_DIRECTORY="/data/www"
+
 function start_nginx {
     sudo nginx -c `realpath nginx.conf`
 }
+
+# Set up files and directories if needed
+if [ ! -d $WWW_DIRECTORY ]; then
+    sudo mkdir $WWW_DIRECTORY
+    chmod 755 $WWW_DIRECTORY
+fi
+
+# Expose all files in ./www
+sudo cp ./www/* /data/www
 
 # Check if Nginx is not running
 if [ ! -f /run/nginx.pid ]; then
@@ -10,4 +21,8 @@ if [ ! -f /run/nginx.pid ]; then
 else
     sudo nginx -s quit
     start_nginx
+fi
+
+if [ $(which google-chrome) ]; then
+    google-chrome http://localhost
 fi
